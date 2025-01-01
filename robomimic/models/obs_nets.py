7c6fdf78ec006f07a11f382d8efed1f8351c727b
An Vuong <andvg3@gmail.com>
2025-01-01 10:23:19 +0700

    Debug

--- robomimic/models/obs_nets.py
+++ robomimic/models/obs_nets.py
@@ -1337,7 +1337,7 @@ class ICL_MIMO_Transformer(Module):
                 assert inputs[obs_group][k].ndim - 2 == len(self.input_obs_group_shapes[obs_group][k])
 
         inputs = inputs.copy()
-        print(inputs["prompt_dict"])
+        print(inputs["prompt"])
         exit()
 
         transformer_encoder_outputs = None
