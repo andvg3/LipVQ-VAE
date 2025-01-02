6a0826ba20c2775a540330bd52c90b5237d1b241
andvg3 <andvg3@gmail.com>
2025-01-02 09:48:06 +0700

    Debug

--- robomimic/scripts/train.py
+++ robomimic/scripts/train.py
@@ -213,7 +213,12 @@ def train(config, device, eval_only=False):
     )
 
     # Inspect dataset
-    print(trainset[0])
+    for batch in trainset[0]:
+        input_batch = dict()
+        input_batch["obs"] = {k: batch["obs"][k][:, 0, :] for k in batch["obs"]}
+        input_batch["goal_obs"] = batch.get("goal_obs", None) # goals may not be present
+        input_batch["actions"] = batch["actions"][:, 0, :]
+        print(input_batch["actions"].data.shape)
     exit()
 
     if config.experiment.validate:
